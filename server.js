4538251fad3dc480495162455924b0b16721d6ce
Sloba92 <sloba92ki@gmail.com>
2026-03-18 13:04:14 +0100

    Pocetak rada na API

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true}));
+app.use("/", require("./routes/home"));
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
