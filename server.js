fc5c3625850edf9f6381d95c3ab409cf0722a947
Sloba92 <sloba92ki@gmail.com>
2026-03-19 15:43:13 +0100

    Postavka baze Mongoose

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const monoose = require("mongoose");
 const PORT = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+monoose.connect("mongodb://127.0.0.1:27017/").then(() => { console.log("MongoDB konektovan"); })
+    .catch((error) => { console.log(error.message) });
+
 app.use("/", require("./routes"));
 
 
