87e9548ee361c94cda63aa5cb21004192f52e6a7
Sloba92 <sloba92ki@gmail.com>
2026-09-17 15:40:23 +0200

    Save-Edited work in progress

--- routes/dnevnici.js
+++ routes/dnevnici.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', require('../controllers/pages/renderDnevnici'));
 router.get('/:id', require('../controllers/pages/renderEditDnevnik'));
 
-router.use('/api/evidencija', require('../controllers/pages/renderEditDnevnikSave'));
+router.use('/evidencija/', require('../controllers/pages/renderEditDnevnikSave'));
 
 router.get('/', (req, res) => {
     res.render('dnevnici');
